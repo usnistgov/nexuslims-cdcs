bbe230b2dfe793bb3ed8568ad9189b7485029b83
Joshua Taillon <joshua.taillon@nist.gov>
2019-09-26 15:59:35 +0000

    initial style changes for NexusLIMS CDCS

--- mdcs_home/views.py
+++ mdcs_home/views.py
@@ -34,16 +34,6 @@ def tiles(request):
         "tiles": []
     }
 
-    if "core_curate_app" in installed_apps:
-        curate_tile = {
-            "logo": "fa-edit",
-            "link": reverse("core_curate_index"),
-            "title": "Curate your Materials Data",
-            "text": "Click here to select a form template and then fill out the corresponding form."
-        }
-
-        context["tiles"].append(curate_tile)
-
     if "core_explore_example_app" in installed_apps:
         explore_example_tile = {
             "logo": "fa-flask",
@@ -52,17 +42,29 @@ def tiles(request):
             "text": "Click here to search for Materials Data in the repository using flexible queries."
         }
 
-        context["tiles"].append(explore_example_tile)
+        # context["tiles"].append(explore_example_tile)
 
     if "core_explore_keyword_app" in installed_apps:
         explore_keywords_tile = {
             "logo": "fa-search",
             "link":  reverse("core_explore_keyword_app_search"),
-            "title": "Search by keyword",
-            "text": "Click here to explore for Materials Data in the repository using keywords."
+            "title": "Browse and Search Records",
+            "text": "Click here to explore the NexusLIMS record repository",
+            "id": "app_search"
         }
 
         context["tiles"].append(explore_keywords_tile)
+
+    if "core_curate_app" in installed_apps:
+        curate_tile = {
+            "logo": "fa-edit",
+            "link": reverse("core_curate_index"),
+            "title": "Create a new record",
+            "text": "Click here to upload a record manually or build a record from scratch",
+            "id": "curator"
+        }
+
+        context["tiles"].append(curate_tile)
 
     if "core_composer_app" in installed_apps:
         compose_tile = {
@@ -72,6 +74,6 @@ def tiles(request):
             "text": "Click here to compose your own template."
         }
 
-        context["tiles"].append(compose_tile)
+        # context["tiles"].append(compose_tile)
 
     return render(request, "mdcs_home/tiles.html", context)
