69d1fb350a0ba871ed533f1452fbb5c9b8eee337
Joshua Taillon <joshua.taillon@nist.gov>
2019-12-20 20:28:21 +0000

    Add menu item for SP calendar (and js to add icon) to top bar

---
+++ static/js/menu_custom.js
@@ -0,0 +1,15 @@
+// This file will add a calendar icon and target='_blank' attribute to 
+// sharepoint calendar link in the menu
+
+// link in top bar:
+$('nav#nav a[href*="msed/MSED-MMF/default.aspx"]').replaceWith(
+    '<a target="_blank" href="https://share.nist.gov/sites/MML/Div/msed/MSED-MMF/default.aspx"><i class="fa fa-calendar"></i><span style="margin-right:5px;"></span>Sharepoint Calendar</a>'
+)
+
+$(document).ready(function() {
+    // link in side bar (when screen is narrow):
+    // only works if you wait until everything has been loaded 
+    // (unlike above for the top menu that works right away)
+    $( "<i class='fa fa-calendar'></i><span style='margin-right:5px;'></span>" ).insertAfter('div#navPanel a[href*="msed/MSED-MMF/default.aspx"]> span');
+});
+
