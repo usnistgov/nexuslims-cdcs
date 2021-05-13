15606d2a946e6ea9a6096e81621836667dad1af2
Joshua Taillon <joshua.taillon@nist.gov>
2021-05-13 05:17:29 +0000

    Update xsl_transform_tag.py to be consistent with upstream changes

--- mdcs_home/templatetags/xsl_transform_tag.py
+++ mdcs_home/templatetags/xsl_transform_tag.py
@@ -5,7 +5,10 @@ from django import template
 from django.contrib.staticfiles import finders
 
 from core_main_app.commons import exceptions
-from core_main_app.components.template_xsl_rendering import api as template_xsl_rendering_api
+from core_main_app.components.template_xsl_rendering import (
+    api as template_xsl_rendering_api,
+)
+from core_main_app.components.xsl_transformation import api as xsl_transformation_api
 from core_main_app.settings import DEFAULT_DATA_RENDERING_XSLT
 from core_main_app.utils.file import read_file_content
 # from core_main_app.utils.xml import xsl_transform
@@ -19,9 +22,9 @@ class XSLType(object):
     type_detail = "Detail"
 
 
-@register.simple_tag(name='xsl_transform_list')
+@register.simple_tag(name="xsl_transform_list")
 def render_xml_as_html(*args, **kwargs):
-    """ Render an XML to HTML using the list xslt.
+    """Render an XML to HTML using the list xslt.
     Args:
         *args:
         **kwargs:
@@ -33,18 +36,26 @@ def render_xml_as_html(*args, **kwargs):
         HTML
 
     """
-    # import web_pdb; web_pdb.set_trace(port=3000)
-
-    xml_content = kwargs.pop('xml_content')
-    template_id = kwargs.pop('template_id', None)
-    template_hash = kwargs.pop('template_hash', None)
+    xml_content = kwargs.pop("xml_content")
+    template_id = kwargs.pop("template_id", None)
+    template_hash = kwargs.pop("template_hash", None)
+    xsl_transform_id = kwargs.pop("xslt_id", None)
+    request = kwargs.pop("request", None)
     detail_url = '"{}"'.format(kwargs.pop('detail_url', '#'))
-    return _render_xml_as_html(xml_content, template_id, template_hash, XSLType.type_list, detail_url=detail_url)
+    return _render_xml_as_html(
+        xml_content,
+        template_id,
+        template_hash,
+        XSLType.type_list,
+        xsl_transform_id,
+        request=request,
+        detail_url=detail_url,
+    )
 
 
-@register.simple_tag(name='xsl_transform_detail')
+@register.simple_tag(name="xsl_transform_detail")
 def render_xml_as_html(*args, **kwargs):
-    """ Render an XML to HTML using the detail xslt.
+    """Render an XML to HTML using the detail xslt.
     Args:
         *args:
         **kwargs:
@@ -53,19 +64,39 @@ def render_xml_as_html(*args, **kwargs):
         HTML
 
     """
-    xml_content = kwargs.pop('xml_content')
-    template_id = kwargs.pop('template_id', None)
-    template_hash = kwargs.pop('template_hash', None)
+    xml_content = kwargs.pop("xml_content")
+    template_id = kwargs.pop("template_id", None)
+    template_hash = kwargs.pop("template_hash", None)
+    xsl_transform_id = kwargs.pop("xslt_id", None)
+    request = kwargs.pop("request", None)
     xmlName = '"{}"'.format(kwargs.pop('xmlName', ''))
-    return _render_xml_as_html(xml_content, template_id, template_hash, XSLType.type_detail, xmlName=xmlName, **kwargs)
+    return _render_xml_as_html(
+        xml_content,
+        template_id,
+        template_hash,
+        XSLType.type_detail,
+        xsl_transform_id,
+        request=request,
+        xmlName=xmlName, 
+        **kwargs
+    )
 
 
-def _render_xml_as_html(xml_string, template_id=None, template_hash=None, xslt_type=XSLType.type_list, **kwargs):
-    """ Render an XML to HTML according to an xslt type (list or detail).
+def _render_xml_as_html(
+    xml_string,
+    template_id=None,
+    template_hash=None,
+    xslt_type=XSLType.type_list,
+    xsl_transform_id=None,
+    request=None,
+    **kwargs
+):
+    """Render an XML to HTML according to an xslt type (list or detail).
     Args:
         xml_string:
         template_id:
         template_hash:
+
         xslt_type:
         kwargs: dict
             Other keyword arguments are passed to the XSLT object as parameters
@@ -76,19 +107,33 @@ def _render_xml_as_html(xml_string, template_id=None, template_hash=None, xslt_t
     """
     try:
         try:
-            if template_id:
-                template_xsl_rendering = template_xsl_rendering_api.get_by_template_id(template_id)
-            elif template_hash:
-                template_xsl_rendering = template_xsl_rendering_api.get_by_template_hash(template_hash)
-            else:
-                raise Exception("No template information provided. Default xslt will be used.")
-
-            if xslt_type == XSLType.type_list:
-                xslt_string = template_xsl_rendering.list_xslt.content
-            elif xslt_type == XSLType.type_detail:
-                xslt_string = template_xsl_rendering.detail_xslt.content
-            else:
+            if xslt_type not in (XSLType.type_list, XSLType.type_detail):
                 raise Exception("XSLT Type unknown. Default xslt will be used.")
+            if xsl_transform_id:
+                xsl_transformation = xsl_transformation_api.get_by_id(xsl_transform_id)
+            elif template_id or template_hash:
+                if template_id:
+                    template_xsl_rendering = (
+                        template_xsl_rendering_api.get_by_template_id(template_id)
+                    )
+                else:
+                    template_xsl_rendering = (
+                        template_xsl_rendering_api.get_by_template_hash(
+                            template_hash, request=request
+                        )
+                    )
+
+                if xslt_type == XSLType.type_list:
+                    xsl_transformation = template_xsl_rendering.list_xslt
+                else:
+                    xsl_transformation = template_xsl_rendering.default_detail_xslt
+            else:
+                raise Exception(
+                    "No template information provided. Default xslt will be used."
+                )
+
+            xslt_string = xsl_transformation.content
+
         except (Exception, exceptions.DoesNotExist):
             default_xslt_path = finders.find(DEFAULT_DATA_RENDERING_XSLT)
             xslt_string = read_file_content(default_xslt_path)
